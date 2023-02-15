4a9ce8a95de9cbc52cf1541ae1e7c9d635a8679f
Lukasz Jurkiewicz <wookash@Lukaszs-MacBook-Pro.local>
2023-02-15 10:22:30 +0000

    generatePassword created, using parameteres set by user create random password and display on the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,6 +51,30 @@ function getPasswordParameters() {
       return true;
   };
 
+function generatePassword() {
+  // If password length is selected correctly and at least one type of characters is being used, run the function.
+  if(getPasswordLength()) if(!getPasswordParameters()) return "Passoword not generated. Try again."; 
+
+  var pwd = "";
+    // First characters of the string to be one from each character type
+    // Personal note: password will be generated without this part, just to make sure random will not fail. 
+  if(pwdParameters.lowercase) pwd += charactersBank.lowercase[Math.floor(Math.random() * charactersBank.lowercase.length)];
+  if(pwdParameters.uppercase) pwd += charactersBank.uppercase[Math.floor(Math.random() * charactersBank.uppercase.length)];
+  if(pwdParameters.numeric) pwd += charactersBank.numeric[Math.floor(Math.random() * charactersBank.numeric.length)];
+  if(pwdParameters.specialChar) pwd += charactersBank.specialChar[Math.floor(Math.random() * charactersBank.specialChar.length)];
+  
+    // Randomly choose type of allowed characters and randomly choose one from the bank - add it to the password string until password length reach length selected by user
+  while(pwd.length < pwdParameters.len) {
+    switch(Math.floor(Math.random() * 4)) {
+      case 0: if(pwdParameters.lowercase) pwd += charactersBank.lowercase[Math.floor(Math.random() * charactersBank.lowercase.length)]; break;
+      case 1: if(pwdParameters.uppercase) pwd += charactersBank.uppercase[Math.floor(Math.random() * charactersBank.uppercase.length)]; break;
+      case 2: if(pwdParameters.numeric) pwd += charactersBank.numeric[Math.floor(Math.random() * charactersBank.numeric.length)]; break;
+      case 3: if(pwdParameters.specialChar) pwd += charactersBank.specialChar[Math.floor(Math.random() * charactersBank.specialChar.length)]; break;
+   };
+  };
+  return pwd;
+};
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
